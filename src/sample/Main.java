e3192e76b752dba1a6deb88f86550c21befcafe0
LainBal <aleponijs@gmail.com>
2020-09-02 20:51:48 +0300

    Added Dice + Game piece, it moves when the dice ir rolled

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,6 +2,9 @@ package sample;
 
 import javafx.animation.PathTransition;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
@@ -18,6 +21,7 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import sample.dice.Dice;
@@ -47,33 +51,7 @@ public class Main extends Application {
 
     private ImageView playerImage = new ImageView(new Image(new File("src/red2.png").toURI().toString()));
 
-    private int move ( int diceValue){
-        int temp = playerPosition + diceValue;
-        if (temp >= NUM_OF_TILE) {
-            temp = NUM_OF_TILE - 1;
-        }
-        Tile tile = tiles.get(temp);
 
-        Path imagePath = new Path();
-        imagePath.getElements().add(
-                new MoveTo(playerImage.getTranslateX() + 10 + playerImage.getFitWidth() / 2,
-                        playerImage.getTranslateY() + 10 + playerImage.getFitHeight() / 2));
-        imagePath.getElements().add(
-                new LineTo(tile.getTranslateX() + 10 + playerImage.getFitWidth() / 2,
-                        tile.getTranslateY() + 10 + playerImage.getFitHeight() / 2));
-
-        PathTransition imageTransition = new PathTransition();
-        imageTransition.setNode(playerImage);
-        imageTransition.setPath(imagePath);
-        imageTransition.setDuration(Duration.seconds(1));
-        imageTransition.play();
-
-        //image.setTranslateX(tile.getTranslateX()+10);
-        //image.setTranslateY(tile.getTranslateY()+10);
-
-        return temp;
-
-    }
     private Parent createContent() throws FileNotFoundException {
         Pane root = new Pane();
         //root.setPrefSize(1000, 700);
@@ -120,7 +98,6 @@ public class Main extends Application {
             if (playerPosition == NUM_OF_TILE - 1) {
 //                endGame;
             }
-
         });
 
         root.getChildren().addAll(dice1, btn, playerImage);
@@ -149,6 +126,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+
 //Texta vizuaja daļa
         VBox vBox = new VBox();
 
@@ -167,7 +145,6 @@ public class Main extends Application {
 
         vBox.getChildren().addAll(label, label1);
 
-
         HBox hBox = new HBox();
         Label label2 = new Label("Score: ");
         label2.setFont(Font.font("Roboto", FontWeight.BOLD, 20));
@@ -178,7 +155,6 @@ public class Main extends Application {
         label3.setPadding(new Insets(10, 10, 50, 10));
 
         hBox.getChildren().addAll(label2, label3);
-
 
         BorderPane borderPane = new BorderPane();
         borderPane.setRight(vBox);
@@ -191,7 +167,74 @@ public class Main extends Application {
         window.setTitle("Game");
         window.show();
     }
-    private void endGame () {
+
+    private int move(int diceValue) {
+        int temp = playerPosition + diceValue;
+        if (temp >= NUM_OF_TILE) {
+            temp = NUM_OF_TILE - 1;
+        }
+        Tile tile = tiles.get(temp);
+
+        Path imagePath = new Path();
+        imagePath.getElements().add(
+                new MoveTo(playerImage.getTranslateX() + 10 + playerImage.getFitWidth() / 2,
+                        playerImage.getTranslateY() + 10 + playerImage.getFitHeight() / 2));
+        imagePath.getElements().add(
+                new LineTo(tile.getTranslateX() + 10 + playerImage.getFitWidth() / 2,
+                        tile.getTranslateY() + 10 + playerImage.getFitHeight() / 2));
+
+        PathTransition imageTransition = new PathTransition();
+        imageTransition.setNode(playerImage);
+        imageTransition.setPath(imagePath);
+        imageTransition.setDuration(Duration.seconds(1));
+
+        //šis event atver jaunu logu, kad beidzas animacija
+        imageTransition.setOnFinished(event -> {
+            newWindow();
+        });
+        imageTransition.play();
+
+        //image.setTranslateX(tile.getTranslateX()+10);
+        //image.setTranslateY(tile.getTranslateY()+10);
+
+        return temp;
+
+    }
+//new window metode
+    private void newWindow() {
+        Label secondLabel = new Label("I'm a Label on new Window");
+        StackPane secondaryLayout = new StackPane();
+        Scene secondScene = new Scene(secondaryLayout, 230, 100);
+
+        // New window (Stage)
+        Stage newWindow = new Stage();
+        newWindow.setTitle("Second Stage");
+        newWindow.setScene(secondScene);
+
+        // Specifies the modality for new window.
+        newWindow.initModality(Modality.WINDOW_MODAL);
+
+        // Specifies the owner Window (parent) for new window
+        newWindow.initOwner(window);
+
+        // Set position of second window, related to primary window.
+        newWindow.setX(window.getX() + 200);
+        newWindow.setY(window.getY() + 100);
+
+        Button secondButton = new Button();
+        secondButton.setText("Close");
+        secondButton.setOnAction(actionEvent -> {
+                    newWindow.close();
+                }
+
+        );
+
+        secondaryLayout.getChildren().addAll(secondButton);
+
+        newWindow.show();
+    }
+
+    private void endGame() {
 
         //jauzraksta metode kas notiek pie pedeja laucina(35) ( piem, atbild uz jautajum un izlec logs "thank you for
         //playing vai kaut ka ta
